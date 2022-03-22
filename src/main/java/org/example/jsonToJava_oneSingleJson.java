117d336eb5b7de870925390abc027ed152829a5b
Josue Gimenez <jgimenez@MacBook-Pro-de-Whisbi.local>
2022-03-22 09:13:24 +0100

    jsonToJava_oneSingleJson created, but it creates with /, has to be fixed on the future

---
+++ src/main/java/org/example/jsonToJava_oneSingleJson.java
@@ -0,0 +1,39 @@
+package org.example;
+
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.File;
+import java.io.IOException;
+import java.sql.*;
+import java.util.ArrayList;
+
+public class jsonToJava {
+    public static void main(String[] args) throws SQLException, IOException {
+        Connection con = null;
+        con = DriverManager.getConnection("jdbc:mysql://localhost:3306/Business","root","Whisbi2022");
+        Statement st = con.createStatement();
+        ResultSet result = st.executeQuery("select * from CustomerInfo where Location ='Asia';");
+
+        ArrayList<customerDetails> customerDetailsArray = new ArrayList<customerDetails>();
+        ObjectMapper om = new ObjectMapper();
+
+        while(result.next()) {
+
+            customerDetails cd = new customerDetails();
+            cd.setCourseName(result.getString(1));
+            cd.setPurchasedDate(result.getString(2));
+            cd.setAmount(result.getInt(3));
+            cd.setLocation(result.getString(4));
+
+            customerDetailsArray.add(cd);
+        }
+
+        for (int i=0; i<customerDetailsArray.size(); i++)
+        om.writeValue(new File("/Users/jgimenez/Josu/SDET project/src/main/java/org/example/customerInfo"+i+".json"), customerDetailsArray.get(i));
+        om.writeValue(new File("/Users/jgimenez/Josu/SDET project/src/main/java/org/example/customerInfoCompletJSON.json"), customerDetailsArray);
+
+        con.close();
+
+    }
+}
